21c13fc81d5f0d03e7e97218677b955e79808a96
Ola <zasada.ola@gmail.com>
2022-04-09 14:28:41 +0200

    added json file

--- testnode.js
+++ testnode.js
@@ -1,8 +1,8 @@
 const http = require('http');
-const port = process.env.PORT || 9000;
+const port = process.env.PORT || 8080;
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end('Gotowanie jest super SUPER!');
+  res.end('Testowanie jest super SUPER!');
 }
 const server = http.createServer(requestListener);
 server.listen(port);
